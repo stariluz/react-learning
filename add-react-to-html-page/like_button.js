cae7cb4e1a68f3388a5dd82028d4a3108945d341
Adrian González <adrigondo.profesional@gmail.com>
2023-01-05 16:54:16 -0600

    Add: creating component for buttons manage

--- add-react-to-html-page/like_button.js
+++ add-react-to-html-page/like_button.js
@@ -1,26 +1,63 @@
 'use strict';
 
-const e = React.createElement;
+const RCE = React.createElement;
 
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {
+      isActive: false
+    };
   }
-
+  onClickButton(){
+    this.setState({
+      isActive:!this.state.isActive,
+    })
+  }
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
 
-    return e(
+    return RCE(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      {
+        onClick: () => {this.onClickButton()},
+        id: this.props.buttonID
+      },
+      // this.props.buttonName,
+      RCE((props)=>{
+        let name=this.props.buttonName
+        if(this.state.isActive){
+          name+=" clicked :)";
+        }
+        return RCE('span',{},name);
+      }),
     );
   }
+}
+function ButtonsContainer(props){
+  return RCE(
+    'div',
+    {},
+    RCE(LikeButton, {buttonID:1, buttonName:"Button A"}),
+    RCE(LikeButton, {buttonID:2, buttonName:"Button B"}),
+    RCE(LikeButton, {buttonID:3, buttonName:"Button C"})
+  );
 }
 
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(RCE(ButtonsContainer));
+
+document.querySelectorAll(".button_container_js").forEach((domContainer)=>{
+  const buttonID=parseInt(domContainer.dataset.button_id);
+  const buttonName=domContainer.dataset.button_name;
+  const root=ReactDOM.createRoot(domContainer);
+  root.render(
+    RCE(
+      LikeButton,
+      {
+        buttonID: buttonID,
+        buttonName: buttonName
+      }
+    )
+  )
+})
